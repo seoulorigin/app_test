1ae02dd0dc4025a02786f2f597eed37e2e37424e
김경원 <seoulorigin@seoulorigin-MacBookAir.local>
2025-05-16 13:26:19 +0900

    디렉토리 정리

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/screens/SearchPage.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,86 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const SearchPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const SearchPage(),
+      },
     );
   }
 }
 
-class SearchPage extends StatelessWidget {
-  const SearchPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final TextEditingController _controller = TextEditingController();
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: [
-          // 중앙 텍스트
-          Center(
-            child: Text(
-              '무엇을 도와드릴까요?',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ),
-          // 하단 입력창
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              color: Colors.white.withOpacity(0.95),
-              child: Row(
-                children: [
-                  // 입력창 (왼쪽부터 최대한 넓게)
-                  Expanded(
-                    child: Container(
-                      height: 48,
-                      alignment: Alignment.centerLeft,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade100,
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      child: TextField(
-                        controller: _controller,
-                        decoration: InputDecoration(
-                          hintText: '무엇이든 부탁하세요',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(24),
-                            borderSide: BorderSide.none,
-                          ),
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                          filled: true,
-                          fillColor: Colors.grey.shade100,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 8),
-                  // 검정색 동그라미 버튼 (위쪽 화살표)
-                  Container(
-                    width: 48,
-                    height: 48,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      icon: Icon(Icons.arrow_upward, color: Colors.white),
-                      onPressed: () {
-                        // 원하는 동작 추가 가능
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
